b864f64485eea772e6ed1d1749716dbc94b69b8e
PriAwe <pricila_awe@yahoo.com.br>
2026-03-15 14:58:24 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file

---
+++ Const.py
@@ -0,0 +1,14 @@
+# C
+COLOR_WHITE = (255, 255, 255)
+COLOR_SILVER = (192, 192, 192)
+
+
+# M
+MENU_OPTION = ('Iniciar Jogo',
+               'Pontuação',
+               'Opções',
+               'Sair')
+
+# W
+WIN_WIDTH = 960
+WIN_HEIGHT = 540
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from Const import WIN_WIDTH, COLOR_WHITE, MENU_OPTION, COLOR_SILVER
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/backgroundMenu.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/menuMusic.wav')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(100, "Space", COLOR_WHITE, ((WIN_WIDTH / 2), 80))
+            self.menu_text(100, "Shooter", COLOR_WHITE, ((WIN_WIDTH / 2), 170))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_SILVER, ((WIN_WIDTH / 2), 410 + 30 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # End game
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.Font("./asset/orbitron-Font.ttf", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
